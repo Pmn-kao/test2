1e60bdea915d21c97574fc01918666d22a36d9e8
Pmn-Kao <kaopmn160942@gmail.com>
2021-12-14 16:14:58 +0700

    git commit

--- src/imports-excel/imports-excel.controller.ts
+++ src/imports-excel/imports-excel.controller.ts
@@ -12,23 +12,27 @@ import {
   UseInterceptors,
 } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
-import { ApiBody, ApiConsumes } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiBody, ApiConsumes, ApiTags } from "@nestjs/swagger";
 import { InjectRepository } from "@nestjs/typeorm";
 import { response } from "express";
+import { JwtAuthGuard } from "src/api/auth/jwt-guard";
 import { Albums } from "src/entity/albums.entity";
 import { Repository } from "typeorm";
 import { ImportsExcelDto } from "./dto/imports-excel.dto";
 import { FileUploadMiddleware } from "./imports-excel.middleware";
+import xlsx from "node-xlsx";
+import { join } from "path";
 
-
+@ApiTags("imports-excel")
+@Controller("imports-exce")
+// @ApiBearerAuth()
+// @UseGuards(JwtAuthGuard)
 @Controller()
 export class UploadController {
   constructor(
-  @InjectRepository(Albums)
-    private readonly albumRepository: Repository<Albums>,
-  ){
-
-  }
+    @InjectRepository(Albums)
+    private readonly albumRepository: Repository<Albums>
+  ) {}
   @Post("upload")
   @ApiBody({ type: ImportsExcelDto })
   @ApiConsumes("multipart/form-data")
@@ -36,13 +40,32 @@ export class UploadController {
     FileInterceptor("file", FileUploadMiddleware(2 * 1024 * 1024, "pmn"))
   )
   async uploadFile(@UploadedFile() file: Express.Multer.File) {
-    console.log(file);
+    // Parse a buffer
+
     if (!file.originalname.match(/\.(xlsx|pdf|xls)$/)) {
       throw new Error("error");
     }
+    console.log(
+      join(__dirname, "../..", "bucket", "uploads", "pmn", file.originalname)
+    );
+
     const album = new Albums();
     album.url = file.path;
     const url = await this.albumRepository.save(album);
     return url;
+  }
+  @Get()
+  async readfile() {
+    const workSheetsFromBuffer = xlsx.parse(
+      join(__dirname, "../..", "bucket", "uploads", "pmn", `รหัสอาคาร.xlsx`)
+    );
+
+    for (let i = 3; i < workSheetsFromBuffer[0].data.length; i++) {
+      console.log(workSheetsFromBuffer[0].data[i][2]);
+      console.log(workSheetsFromBuffer[0].data[i][5]);
+      console.log(workSheetsFromBuffer[0].data[i][6]);
+      console.log(workSheetsFromBuffer[0].data[i][8]);
+      console.log(workSheetsFromBuffer[0].data[i][9]);
+    }
   }
 }
